28d7ddfbbd0832c28e40354a0f40146afe04441c
mikemoreira <mikemoreira@gmail.com>
2016-12-08 15:17:31 -0200

    Tentativa de recuperar o VCAP_SERVICES no maven

--- src/main/test/br/acem/ms/elegibilidade/api/res/ElegibilidadeResourcesTest.java
+++ src/main/test/br/acem/ms/elegibilidade/api/res/ElegibilidadeResourcesTest.java
@@ -4,37 +4,33 @@ import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.get;
 import static org.hamcrest.Matchers.equalTo;
 
-import java.util.Map;
-
 import org.junit.Test;
+
+import com.ibm.json.java.JSON;
+import com.ibm.json.java.JSONObject;
 
 import io.restassured.RestAssured;
 
 public class ElegibilidadeResourcesTest {
-	
-	
-	public ElegibilidadeResourcesTest(){
-		//Local
-		//RestAssured.baseURI =  "http://localhost:9080/elegibilidade";
-		//Bluemix 
-		RestAssured.baseURI =  "http://elegibilidade.mybluemix.net";
-		
-		Map env = System.getenv();
-		String host = (String)env.get("VCAP_APP_HOST");
-		String port = (String)env.get("VCAP_APP_PORT");
-		
-		System.out.println("Hello from " + host + ":" + port);
-	}
-	
 
-	
+	public ElegibilidadeResourcesTest() {
+		// Local
+		// RestAssured.baseURI = "http://localhost:9080/elegibilidade";
+		// Bluemix
+		RestAssured.baseURI = "http://elegibilidade.mybluemix.net";
+		try {
+			JSONObject vcap = (JSONObject) JSON.parse(System.getenv("VCAP_SERVICES"));
+
+			System.out.println("Hello from " + vcap.toString());
+		} catch (Exception excEnv) {
+			System.out.println("VCAP Não Encontrado!");
+		}
+	}
+
 	@Test
 	public void testeSimples() {
 		System.out.println(baseURI);
-		get("/validarElegibilidade/123")
-		.then()
-			.statusCode(200)
-			.body("valor", equalTo("Calculado GET"));
+		get("/validarElegibilidade/123").then().statusCode(200).body("valor", equalTo("Calculado GET"));
 
 	}
 }
